9fcaab21b8be37b93d2a3d348ed6e6dac8e7a904
Masashi Kayahara <x.masashi.kayahara@intloop.com>
2021-01-20 11:10:43 +0900

    First commit.

---
+++ src/main/java/com/yukiusa/autojavaformat/AutoJavaFormatApplication.java
@@ -0,0 +1,13 @@
+package com.yukiusa.autojavaformat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoJavaFormatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoJavaFormatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yukiusa/autojavaformat/AutoJavaFormatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukiusa.autojavaformat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoJavaFormatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
