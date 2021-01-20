287b388ed280c9aa9bb5b319234b1d4cb478e206
yukiusa <yukiusa@arc-connects.com>
2021-01-20 07:31:45 +0000

    Automated :Reformat Java source code.

--- src/main/java/com/yukiusa/autojavaformat/AutoJavaFormatApplication.java
+++ src/main/java/com/yukiusa/autojavaformat/AutoJavaFormatApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AutoJavaFormatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutoJavaFormatApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AutoJavaFormatApplication.class, args);
+  }
 }
